6741c217fc3effb0b517acde8a254e28c68379af
Petrea Stefan <stefan.petrea20@gmail.com>
2023-09-14 17:55:38 +0300

    Done funct edit & save btn

--- app.js
+++ app.js
@@ -9,21 +9,22 @@ function createCard(name){
     cardContainer.className ="card-container"
 
     let invitee = document.createElement("p")
-    invitee.textContent = name
-    invitee.style.margin="0px"
-    invitee.style.fontFamily="Roboto"
-    invitee.style.fontSize="20px"
-    cardContainer.append(invitee)
+    invitee.className = "invitee-name"
+    invitee.textContent = name;
+    invitee.style.margin="0px";
+    invitee.style.fontFamily="Roboto";
+    invitee.style.fontSize="20px";
+    cardContainer.append(invitee);
 
-    let checkBoxContainer = document.createElement("div")
-    checkBoxContainer.className="checkbox-label-container"
+    let checkBoxContainer = document.createElement("div");
+    checkBoxContainer.className="checkbox-label-container";
 
     let confirmedCheckBox = document.createElement("input");
-    confirmedCheckBox.type = "checkbox"
-    confirmedCheckBox.id = "confirmed-checkbox"
-    let checkBoxLabel= document.createElement("label")
-    checkBoxLabel.textContent="Confirmed"
-    checkBoxLabel.setAttribute("for","confirmed-checkbox")
+    confirmedCheckBox.type = "checkbox";
+    confirmedCheckBox.id = "confirmed-checkbox";
+    let checkBoxLabel= document.createElement("label");
+    checkBoxLabel.textContent="Confirmed";
+    checkBoxLabel.setAttribute("for","confirmed-checkbox");
 
     checkBoxContainer.append(confirmedCheckBox);
     checkBoxContainer.append(checkBoxLabel);   
@@ -35,7 +36,7 @@ function createCard(name){
     editBtn = document.createElement("button");
     editBtn.textContent = "Edit";
     editBtn.className= "button-style";
-    editBtn.className += " edit-btn"
+    editBtn.className += " edit-btn";
 
     removeBtn = document.createElement("button");
     removeBtn.textContent = "Remove";
@@ -119,9 +120,48 @@ invContainer.addEventListener("click",(e)=>{
         invContainer.removeChild(card)
     }
 
+    if(classes.contains("edit-btn")){
+        let para = card.firstChild;
 
 
+        let inpt = document.createElement("input");
+        inpt.style.width="135px"
+        inpt.style.fontSize="20px"
+        inpt.setAttribute("type","text");
+        inpt.value = para.textContent
 
+        card.insertBefore(inpt,para);
+
+        card.removeChild(para);
+
+        object.classList.remove("edit-btn");
+
+        object.classList.add("save-btn");
+
+        object.textContent = "Save";
+    }
+
+    else if(classes.contains("save-btn")){
+
+        let inpt = card.firstElementChild;
+        let para = document.createElement("p");
+        para.className = "invitee-name";
+
+        para.textContent=inpt.value
+
+        card.insertBefore(para,inpt)
+
+        card.removeChild(inpt)
+
+        object.classList.remove("save-btn");
+        object.classList.add("edit-btn");
+        object.textContent="Edit"
+
+        para.style.margin="0px";
+        para.style.fontFamily="Roboto";
+        para.style.fontSize="20px";
+
+    }
 
 
 })
